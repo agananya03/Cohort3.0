c78fb25834b16b2b170392425cd26f6e3a9cf587
agananya03 <agananya54126@gmail.com>
2025-05-30 19:02:40 +0530

    week2

---
+++ week2/class.js
@@ -0,0 +1,26 @@
+class rectangle{
+    constructor(width,height,color){
+        this.width=width
+        this.height=height
+        this.color=color
+    }
+    area(){
+        const area=this.width*this.height
+        return area;
+    }
+    paint(){
+        console.log('painting with color ${this.color}');
+    }
+}
+const rect=new rectangle(2,6);
+const area=rect.area()
+console.log(area);
+
+const now=new Date();
+console.log(now);
+
+const map=new Map();
+map.set('name','Ananya')
+map.set('age',19)
+console.log(map.get('name'))
+
