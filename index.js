796457917324980f0b0bf6a475d9aaf61b6f4454
agananya03 <agananya54126@gmail.com>
2025-06-03 21:17:47 +0530

    nodedocs

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function sum(a,b){
-    return a+b;
-}
-let ans=sum(5,10)
-console.log(ans);g
+const fs=require('fs');
+const {Command}=require('commander');
+const program=new Command();
+
+program
+.name('counter')
+.description('CLI to do file based tasks')
+.version('0.8.0');
+
+program.Command('count')
+ .description('count the number of lines in a file')
+ .argument('<file>','file to count')
+ .action((file)=>{
+    fs.readFile(file,'utf-8',(err,data)=>{
+        if(err){
+            console.log(err);
+        }else{
+            const lines=data.split(' ').length;
+            console.log('There are ${lines} lines in ${file}');
+        }
+    });
+});
+program.parse();
---
+++ week4/index.mjs
@@ -0,0 +1,4 @@
+import chalk from 'chalk'
+console.log(chalk.blue("hello everyone"));
+console.log(chalk.red.bold("this is error"))
+console.log(chalk.green.underline("hi evrybody i am ananya"));
