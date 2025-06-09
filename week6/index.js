500ac7dc1cf630cbfd7849599eb62b6a49332077
agananya03 <agananya54126@gmail.com>
2025-06-09 21:39:44 +0530

    week6

--- week6/index.js
+++ week6/index.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const app=express();
+const jwt=require('jsonwebtoken'); 
+
+const JWT_SECRET="randomtext";
 
 app.use(express.json());
 const users=[];
 
-function generatetoken(){
-    //generate a random token for user after every sign in
-}
+// function generatetoken(){
+//     //generate a random token for user after every sign in
+// }
 
 app.post('/signup',function(req,res){
 const username=req.body.username;
@@ -34,20 +37,46 @@ if(users[i].username==username && users[i].password==password){
 }
 }
 if(founduser){
-    const token=generatetoken();
-    founduser.token=token;
+    const token=jwt.sign({  //converted the username to jwt
+        username: username
+    }, JWT_SECRET);
+
+    //founduser.token=token;
     res.json({
-        message: token
+        token: token
     })
 }else{
     res.status(403).send({
         message:" invalid username or password"
     })
 }
+console.log(users);
 // const user=users.find(function(u){
 // if(u.username==username) return true;
 // else return false;
 // })
+ })
+
+ app.get('/me',function(req,res){
+    const token=req.headers.token;
+    const decodedInformation=jwt.verify(token , JWT_SECRET); //converted jwt to  username
+    const username = decodedInformation.username;
+    let founduser=null;
+    for(let i=0;i<users.length;i++){
+        if(users[i].username == username){
+            founduser=users[i];
+        }
+    }
+    if(founduser){
+        res.json({
+            username: founduser.username,
+            password: founduser.password
+        })
+    } else{
+        res.json({
+            message:"token invalid"
+        })
+    }
  })
 
 app.listen(3000);
