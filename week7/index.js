e77f9fcf01a9a53a778a0f4a3af55eb25803f35e
agananya03 <agananya54126@gmail.com>
2025-06-12 12:59:18 +0530

    add

--- week7/index.js
+++ week7/index.js
@@ -3,7 +3,11 @@ const {UserModel, TodoModel} = require('./db');
 const app=express();
 const jwt=require('jsonwebtoken');
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://ananya03:ananya03@cluster0.gpuf17o.mongodb.net/todo-app-database");
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error(err));
+//mongoose.connect("mongodb+srv://ananya03:ananya03@cluster0.gpuf17o.mongodb.net/todo-app-database");
 app.use(express.json());
 const JWT_SECRET="ananya30";
 
@@ -35,7 +39,7 @@ app.post('/signin', async function(req,res){
     console.log(user);
     if(user){
         const token =jwt.sign({
-            id: user._id
+            id: user._id.toString()
         },JWT_SECRET);
         res.json({
              token
@@ -48,12 +52,41 @@ app.post('/signin', async function(req,res){
 
 });
 
-app.post('/todo', function(req,res){
-
+app.post('/todo',auth, function(req,res){
+    const userID=req.userID;
+    const title=req.body.title;
+    const done=req.body.done;
+    TodoModel.create({
+        title,
+        userID,
+        done
+    })
+    res.json({
+        userID: userID
+    })
 });
 
-app.get('/todos', function(req,res){
-
+app.get('/todos',auth, async function(req,res){
+    const userID=req.userID;
+    const todos =await TodoModel.find({
+        userID: userID
+    })
+    req.json({
+       todos
+    })
 });
+
+function auth(req,res,next){
+const token=req.headers.token;
+const decodeddata=jwt.verify(token,JWT_SECRET);
+if(decodeddata){
+    req.userID= decodeddata.id;
+    next();
+} else{
+    req.status(403).json({
+        message:"Incorrect credentials"
+    })
+}
+}
 
 app.listen(3000);
