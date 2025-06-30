b7c3f463d2687d33ab96769dbcac7e46d01779a2
agananya03 <agananya54126@gmail.com>
2025-06-30 21:09:56 +0530

    week-11

--- week11/week-11-recoil/week-11recoil/src/App.jsx
+++ week11/week-11-recoil/week-11recoil/src/App.jsx
@@ -1,61 +1,87 @@
 import { useEffect, useState, memo } from 'react'
 import './App.css'
-import { useRecoilValue } from 'recoil';
-import { counteratom } from './store/counter';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { jobskatom, messageatom, networkatom, notificationsatom, totalNotificationSelector } from './store/counter';
+
+// function App() {
+
+//   return (
+//     <>
+//     </>
+//   )
+// }
+
+// function counter() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     setInterval(() => {
+//       setCount(c => c + 1)
+//     }, 3000)
+//   }, []);
+
+//   return (
+//     <div>
+//       < Currentcount />
+//         <Increase />
+//         <Decrease />
+//     </div>
+//   )
+// }
+
+// const Currentcount = memo(function() {
+// return <div>
+
+// </div>
+// })
+
+// const Increase = memo(function() {
+//   function increase(){
+
+//   }
+//   return <div>
+//     <button onClick={increase}>Increase</button>
+//   </div>
+// })
+
+// const Decrease = memo(function() {
+//   function decrease(){
+
+//   }
+//   return <div>
+//     <button onClick={decrease}>Decrease</button>
+//   </div>
+// })
+
+// function isEven(){
+//   const even = useRecoilValue(evenselector)
+//   return <div>
+//     {even ? "Even" : "Odd"}
+//   </div>
+// }
+
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+return <RecoilRoot>
+        <Mainapp/>
+      </RecoilRoot>
 }
 
-function counter() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    setInterval(() => {
-      setCount(c => c + 1)
-    }, 3000)
-  }, []);
+function Mainapp() {
+  const networkNotificationCount = useRecoilValue(networkatom);
+  const jobCount = useRecoilValue(jobskatom);
+  const MessagesCount = useRecoilValue(messageatom);
+  const NotificationCount = useRecoilValue(notificationsatom);
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
-  return (
-    <div>
-      < Currentcount />
-        <Increase />
-        <Decrease />
-    </div>
-  )
-}
-
-const Currentcount = memo(function() {
-return <div>
-
-</div>
-})
-
-const Increase = memo(function() {
-  function increase(){
-
-  }
-  return <div>
-    <button onClick={increase}>Increase</button>
-  </div>
-})
-
-const Decrease = memo(function() {
-  function decrease(){
-
-  }
-  return <div>
-    <button onClick={decrease}>Decrease</button>
-  </div>
-})
-
-function isEven(){
-  const even = useRecoilValue(evenselector)
-  return <div>
-    {even ? "Even" : "Odd"}
-  </div>
+return( 
+<>
+  <button>Home</button>
+  <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+  <button>Jobs ({jobCount})</button>
+  <button>Messages ({MessagesCount})</button>
+  <button>Notifications ({NotificationCount})</button>
+  <button> Me ({totalNotificationCount}) </button>
+</>
+)
 }
 export default App
--- week11/week-11-recoil/week-11recoil/src/store/counter.js
+++ week11/week-11-recoil/week-11recoil/src/store/counter.js
@@ -1,14 +1,34 @@
 import { atom, selector } from "recoil";
-export const counteratom = atom({
-    key: "counter",
-    default: 0
-})
+// export const counteratom = atom({
+//     key: "counter",
+//     default: 0
+// })
 
-export const evenselector = selector({
-    key: "isEvenSelector",
-    get: function({get}) {
-        const currentcount = get(counteratom);
-        const isEven = (currentcount % 2 == 0);
-        return isEven;
-    }
+// export const evenselector = selector({
+//     key: "isEvenSelector",
+//     get: function({get}) {
+//         const currentcount = get(counteratom);
+//         const isEven = (currentcount % 2 == 0);
+//         return isEven;
+//     }
+// })
+
+export const networkatom = atom({
+    key: "networkatom",
+    default: 103
+});
+
+export const jobskatom = atom({
+    key: "jobsatom",
+    default: 0
+});
+
+export const notificationsatom = atom({
+    key: "notificationsatom",
+    default: 12
+});
+
+export const messageatom = atom({
+    key: "messageatom",
+    default: 0
 })
