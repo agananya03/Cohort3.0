cbc858c52e0d199fc5f2ed41f099fa07f4a1b226
agananya03 <agananya54126@gmail.com>
2025-07-16 20:34:58 +0530

    add

--- week-14/src/index.ts
+++ week-14/src/index.ts
@@ -1,1 +1,18 @@
-let x: number = 1
+interface user {
+    id: string,
+    name: string,
+    age: string,
+    password: string,
+    email: string
+}
+type updateProps = Pick<user, 'name' | 'age' | 'email'>
+
+type updatePropsOptional = Partial<updateProps> //optionally passing
+function updateUser(updatedProps: updatePropsOptional) {
+//hit the database to update the user
+}
+
+//readonly
+type user1 = {
+    name: string
+}
