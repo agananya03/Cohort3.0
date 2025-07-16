b30a49da448a139132e8f296db26589ef54e9caa
agananya03 <agananya54126@gmail.com>
2025-07-16 11:21:16 +0530

    add

--- Week-13/src/components/answers/basic-project.jsx
+++ Week-13/src/components/answers/basic-project.jsx
@@ -1,6 +1,6 @@
 export function Sidebarclass1() {
     return <div className="flex">
-        <div className="transition-all delay-1000 bg-blue-500 w-0 md:w-96 md:translate-x-0 -translate-x-96 h-screen ">
+        <div className="transition-all delay-1000 duration-200 bg-blue-500 w-0 md:w-96 h-screen ">
             Sidebar
         </div>
         <div className="bg-green-400 w-full h-screen">
