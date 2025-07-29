3487550090b9731c07bba72d969b39dd69ed9128
agananya03 <agananya54126@gmail.com>
2025-07-30 00:22:14 +0530

    add

--- week-16/src/index.ts
+++ week-16/src/index.ts
@@ -1,11 +1,27 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 
 const wss = new WebSocketServer({port: 8080});
 
-wss.on("connection", function(socket) {
-    socket.on("message", (e)=>{
-        if(e.toString() === "ping"){
-            socket.send("pong");
+// wss.on("connection", function(socket) {
+//     socket.on("message", (e)=>{
+//         if(e.toString() === "ping"){
+//             socket.send("pong");
+//         }
+//     })
+// })
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+wss.on("connection", function(socket){
+    allSockets.push(socket);
+    userCount = userCount + 1;
+
+    socket.on("message", (message)=> {
+        for(let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from the server");
         }
+    })
+    socket.on("disconnect", ()=>{
+        allSockets = allSockets.filter(x => x != socket)
     })
 })
