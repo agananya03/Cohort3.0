16eecde229e0e88e0ee368f873d0b00db4c2ea47
agananya03 <agananya54126@gmail.com>
2025-08-05 21:23:21 +0530

    week-17

---
+++ week-17/src/index.ts
@@ -0,0 +1,36 @@
+
+import express from "express";
+import { Client } from "pg";
+
+const app = express();
+app.use(express.json());
+
+const pgClient = new Client("//url")
+
+pgClient.connect();
+
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    try {
+
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`
+
+        const response = await pgClient.query(insertQuery, [username, email, password]);
+
+        res.json({
+            message: "You have signed up"
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            message: "Error while signing up"
+        })
+    }
+
+})
+
+app.listen(3000)
