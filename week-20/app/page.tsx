d35d18a1fba24f9bf6180ff12978c802c81c8aa1
agananya03 <agananya54126@gmail.com>
2025-08-18 20:30:59 +0530

    week-21

--- week-20/app/page.tsx
+++ week-20/app/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
-  const session = useSession()
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      
-    </div>
-  );
+export default async function Home() {
+  const session = await getServerSession()
+  const userProfile = db.avatars.findOne({
+    where: {
+     
+    }
+  })
+  return <div>
+    {JSON.stringify(userProfile.avatar)}
+  </div>
 }
